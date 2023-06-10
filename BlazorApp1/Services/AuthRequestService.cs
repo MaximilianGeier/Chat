583fcc0aca387fb8b8fd076d330b53def833f31f
geer.max0404 <geer.max0404@gmail.com>
2023-06-11 01:30:32 +0500

    Added Auth method

--- BlazorApp1/Services/AuthRequestService.cs
+++ BlazorApp1/Services/AuthRequestService.cs
@@ -49,7 +49,7 @@ public class AuthRequestService
 
         throw new Exception("Not authorized!");
     }
-    
+
     public async Task<HttpResponseMessage?> MakeAuthorizedGet(string url)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, url);
@@ -83,6 +83,22 @@ public class AuthRequestService
     public async Task<HttpResponseMessage?> MakeAuthorizedDelete<T>(string url, T data)
     {
         var request = new HttpRequestMessage(HttpMethod.Delete, url);
+        request.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+        AddAuthCookiesToRequest(request, url);
+        
+        if (_httpContextAccessor.HttpContext.Request.Cookies.Select(pair => pair.Key).Any(key => key.Contains(".AspNetCore.Antiforgery")) &&
+            _httpContextAccessor.HttpContext.Request.Cookies.ContainsKey(".AspNetCore.Identity.Application"))
+        {
+            var response = await _httpClient.SendAsync(request);
+            return response;
+        }
+
+        throw new Exception("Not authorized!");
+    }
+    
+    public async Task<HttpResponseMessage?> MakeAuthorizedPatch<T>(string url, T data)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Patch, url);
         request.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
         AddAuthCookiesToRequest(request, url);
         
