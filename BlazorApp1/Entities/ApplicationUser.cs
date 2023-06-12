4a6aab70de17bc8dd03f74bb6772ee3824942a6b
IlyaIIS <ilya1221221@mail.ru>
2023-06-12 18:56:40 +0500

    Migration fixed

--- BlazorApp1/Entities/ApplicationUser.cs
+++ BlazorApp1/Entities/ApplicationUser.cs
@@ -1,10 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace BlazorApp1.Entities;
 
 public class ApplicationUser : IdentityUser
 {
+    [InverseProperty(nameof(Chatroom.Users))]
     public ICollection<Chatroom> Chatrooms { get; set; } = new List<Chatroom>();
     public bool IsDeleted { get; set; }
+    [InverseProperty(nameof(Chatroom.Admins))]
+    public ICollection<Chatroom> AdminOf { get; set; } = new List<Chatroom>();
 }
